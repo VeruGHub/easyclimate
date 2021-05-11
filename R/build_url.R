a0376f0573c12f8b862a4cdaee71e4a1072fedc2
Pakillo <f.rodriguez.sanc@gmail.com>
2021-05-11 16:41:32 +0200

    revise "2_url.R" -> build_url.R

---
+++ R/build_url.R
@@ -0,0 +1,45 @@
+
+#' Build url address for a given request
+#'
+#' Build the url to download climatic data from ftp://palantir.boku.ac.at/Public/ClimateData/
+#'
+#' @param climatic_var Character. Climatic variable to be downloaded. One of 'Tmax', 'Tmin' or 'Prcp'.
+#' @param year Numeric. Year to download climatic information
+#'
+#' @return text string with the url
+#'
+#' @examples
+#' @author Veronica Cruz-Alonso, Sophia Ratcliffe, Francisco Rodríguez-Sánchez
+
+build_url <- function(climatic_var,
+                      year) {
+
+  # Check arguments
+  if (!climatic_var %in% c("Tmax", "Tmin", "Prcp"))
+    stop("climatic_var must be one of 'Tmax', 'Tmin' or 'Prcp'")
+
+  if (year < 1950 | year > 2017)
+    stop("Year must be between 1950 and 2017")
+
+  # Adjust climvar to file names in FTP server
+  climvar <- switch(climatic_var,
+                    "Tmax" = "tmax",
+                    "Tmin" = "tmin",
+                    "Prcp" = "prec")
+
+  # Build url
+  url <- paste("ftp://palantir.boku.ac.at/Public/ClimateData/v2_cogeo/AllDataRasters/",
+               climvar,
+               "/Downscaled",
+               climatic_var,
+               year,
+               "_cogeo.tif",
+               sep = "")
+
+  invisible(url)
+
+}
+
+
+
+

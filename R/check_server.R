008bde8abd3f719825abdf3a8411fef2be090afe
Francisco Rodriguez-Sanchez <f.rodriguez.sanc@gmail.com>
2022-10-27 19:02:25 +0200

    check_server: allow to pass climatic_var and year [skip ci]

--- R/check_server.R
+++ R/check_server.R
@@ -2,6 +2,9 @@
 #'
 #' Check that the online climate data server is available and working correctly.
 #'
+#' @param climatic_var Optional. One of "Prcp", "Tmin", or "Tmax".
+#' @param year Optional. Year between 1950 and 2020.
+#'
 #' @return TRUE if the server seems available, FALSE otherwise.
 #' @export
 #'
@@ -9,9 +12,18 @@
 #' \dontrun{
 #' check_server()
 #' }
-check_server <- function() {
+check_server <- function(climatic_var = NULL, year = NULL) {
 
-  cog.url <- build_url(climatic_var_single = "Prcp", year = 2010)
+  if (is.null(climatic_var)) {
+    climatic_var <- sample(c("Prcp", "Tmin", "Tmax"), size = 1)
+  }
+
+  if (is.null(year)) {
+    year <- sample(1950:2020, size = 1)
+  }
+
+  cog.url <- build_url(climatic_var_single = climatic_var,
+                       year = year)
 
   server.ok <- RCurl::url.exists(cog.url)
 

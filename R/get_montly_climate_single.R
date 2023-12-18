43e00ce4cc5b987dff576641415556900c81d213
VeruGHub <verukigoku@hotmail.com>
2023-12-18 17:10:47 +0100

    get monthly climate for one variable

---
+++ R/get_montly_climate_single.R
@@ -0,0 +1,320 @@
+
+#' Get monthly data for one climatic variable
+#'
+#' Extract monthly climate data (temperature or precipitation) for a given set of
+#' points or polygons within Europe.
+#'
+#' @param climatic_var_single Character. Climatic variable to be downloaded.
+#' One of 'Tmax', 'Tmin' or 'Prcp'.
+#' @param output Character. Either "df", which returns a dataframe with monthly
+#' climatic values for each point/polygon, or "raster", which returns a
+#' [terra::SpatRaster()] object.
+#' @param check_conn Logical. Check the connection to the server before
+#' attempting data download?
+#' @inheritParams get_montly_climate
+#'
+#' @return A data.frame (if output = "df") or a [terra::SpatRaster()] object
+#' (if output = "raster").
+#'
+#' @keywords internal
+#' @noRd
+#'
+#' @references
+#' Pucher C. 2023. Description and Evaluation of Downscaled Daily Climate Data Version 4.
+#' https://doi.org/10.6084/m9.figshare.22962671.v1
+#'
+#' Werner Rammer, Christoph Pucher, Mathias Neumann. 2018.
+#' Description, Evaluation and Validation of Downscaled Daily Climate Data Version 2.
+#' ftp://palantir.boku.ac.at/Public/ClimateData/
+#'
+#' Adam Moreno, Hubert Hasenauer. 2016. Spatial downscaling of European climate data.
+#' International Journal of Climatology 36: 1444–1458.
+#'
+#' @author Veronica Cruz-Alonso, Francisco Rodriguez-Sanchez
+
+
+get_monthly_climate_single <- function(coords = NULL,
+                                       climatic_var_single = "Prcp",
+                                       period = NULL,
+                                       output = "df",
+                                       version = 4,
+                                       check_conn = TRUE) {
+
+  #### Check arguments ####
+
+  ## version
+  if (!version %in% c(4, 3)) {
+    stop("version must be 3 or 4")
+  }
+
+
+  ## climatic_var_single
+  if (!climatic_var_single %in% c("Tmax", "Tmin", "Prcp")) {
+    stop("climatic_var_single must be one of 'Tmax', 'Tmin' or 'Prcp'")
+  }
+
+  if (climatic_var_single %in% names(coords)) {
+    stop("Coords cannot have a column with the same name as ", climatic_var_single,
+         ". Please change it.")
+  }
+
+  ## coords
+  if (!inherits(coords, c("matrix", "data.frame", "sf", "sfc", "sfg", "SpatVector")))
+    stop("Coords must be either a matrix, data.frame, tbl_df, sf or SpatVector object")
+
+  if (inherits(coords, "matrix")) {
+    stopifnot(ncol(coords) == 2)
+  }
+
+  if (inherits(coords, "data.frame") & !inherits(coords, "sf")) {
+    stopifnot("lon" %in% names(coords))
+    stopifnot("lat" %in% names(coords))
+  }
+
+  if ("ID_coords" %in% names(coords)) {
+    warning("The variable ID_coords will be overwritten. Consider renaming it")
+  }
+
+  #### Convert matrix, data.frame, sf to SpatVector ####
+
+  if (!inherits(coords, "SpatVector")) {
+    coords.spatvec <- terra::vect(coords)
+  } else {
+    coords.spatvec <- coords
+  }
+
+  # If missing CRS, assume lonlat (EPSG:4326)
+  if (terra::crs(coords.spatvec) == "") {
+    warning("Coordinate Reference System is missing and EPSG 4326 is assigned by default")
+    terra::crs(coords.spatvec) <- "epsg:4326"
+  }
+
+  stopifnot(terra::crs(coords.spatvec, describe = TRUE)$code == "4326")
+
+
+  ## Add ID variable
+  coords.spatvec$ID_coords <- seq(from = 1, to = nrow(coords.spatvec), by = 1)
+
+  ## Warn (or stop) if asking data for too many points or too large area
+  # so as not to saturate FTP server
+  if (nrow(coords.spatvec) > 100000) {  # change limits if needed
+    stop("Asking for climate data for >100000 sites. Please reduce the number of sites or download original rasters directly from ftp://palantir.boku.ac.at/Public/ClimateData/ so as not to saturate the server")
+  }
+
+  if (terra::geomtype(coords.spatvec) == "polygons") {
+
+    if (sum(suppressWarnings(terra::expanse(coords.spatvec, unit = "km"))) >
+        100000) {  # change limits if needed
+      stop("Asking for climate data for too large area. Please reduce the polygon area or download original rasters directly from ftp://palantir.boku.ac.at/Public/ClimateData/ so as not to saturate the server")
+    }
+
+  }
+
+  ## Check that SpatVector extent is within bounds
+  if (terra::ext(coords.spatvec)$xmin < -40.5 |
+      terra::ext(coords.spatvec)$xmax > 75.5 |
+      terra::ext(coords.spatvec)$ymin < 25.25 |
+      terra::ext(coords.spatvec)$ymax > 75.5) {
+    stop("Coordinates fall out of the required extent (-40.5, 75.5, 25.25, 75.5)")
+  }
+
+
+  #### Period ####
+
+  ## Convert to dates
+  months <- period_to_months(period)
+
+  ## Extract years
+  years <- as.numeric(sort(unique(format(months, "%Y"))))
+
+  ## Check years are within bounds
+  if (version == 3) {
+    if (any(years < 1950 | years > 2020))
+      stop("Year (period) must be between 1950 and 2020")
+  }
+  if (version == 4) {
+    if (any(years < 1950 | years > 2022))
+      stop("Year (period) must be between 1950 and 2022")
+  }
+
+  #### Build urls for all required years ####
+
+  urls <- unlist(lapply(years,
+                        build_url,
+                        climatic_var_single = climatic_var_single,
+                        version = version,
+                        temp_res = "month"))
+
+  urls <- urls[[1]]
+  urls_NA <- urls[[2]]
+
+  ## Check if the server is working
+  if (isTRUE(check_conn)) {
+    if (isTRUE(check_server(verbose = FALSE))) {
+      message("Connecting to the server...")
+    } else {
+      message("Problems retrieving the data. Please run 'check_server()' to diagnose the problems.\n")
+    }
+  }
+  ###
+
+  urls.vsicurl <- paste0("/vsicurl/", urls)
+  urls_NA.vsicurl <- paste0("/vsicurl/", urls_NA)
+
+  #### Connect and combine all required rasters ####
+
+  ras.list <- lapply(urls.vsicurl, terra::rast)
+  ras_NA.list <- lapply(urls_NA.vsicurl, terra::rast)
+
+  ## Name raster layers with their dates
+  for (i in seq_along(years)) {
+     ras.list.names <- seq.Date(from = as.Date(paste0(years[i], "-01-01")),
+                                     to = as.Date(paste0(years[i], "-12-01")),
+                                     by = "month")
+  names(ras.list[[i]]) <- ras.list.names
+  names(ras_NA.list[[i]]) <- ras.list.names
+
+  }
+
+  ## Combine all years
+  rasters <- ras.list[[1]]
+  if (length(ras.list) > 1) {
+    for (i in 2:length(ras.list)) {
+      terra::add(rasters) <- ras.list[[i]]
+    }
+  }
+
+  rasters_NA <- ras_NA.list[[1]]
+  if (length(ras_NA.list) > 1) {
+    for (i in 2:length(ras_NA.list)) {
+      terra::add(rasters_NA) <- ras_NA.list[[i]]
+    }
+  }
+
+
+  ## Subset required dates only
+  rasters.sub <- terra::subset(rasters, subset = as.character(months))
+  rasters_NA.sub <- terra::subset(rasters_NA, subset = as.character(months))
+
+  #### Extract ####
+
+  message(paste0("\nDownloading ", climatic_var_single,
+                 " data... This process might take several minutes"))
+
+  if (output == "df") {
+
+    out <- terra::extract(rasters.sub, coords.spatvec, xy = TRUE)
+    out_NA <- terra::extract(rasters_NA.sub, coords.spatvec, xy = TRUE)
+
+    ## Reshape to long format and join with coverage
+    out <- reshape_terra_extract(out, climvar = climatic_var_single)
+    out_NA <- reshape_terra_extract(out_NA, climvar = climatic_var_single)
+    names(out_NA)[names(out_NA) == climatic_var_single] <- paste0("daily_", climatic_var_single, "_NA")
+
+    out <- merge(out, out_NA)
+
+    ## Merge with original coords data
+    if (terra::is.polygons(coords.spatvec)) {
+      # if polygons, keep coordinates from raster cells
+      coords.spatvec.df <- terra::as.data.frame(coords.spatvec)
+    } else {
+      # else (ie for point coordinates) keep input coords rather than raster cells
+      out <- subset(out, select = -c(lon, lat))
+      spatvec.coords <- terra::crds(coords.spatvec, df = TRUE)
+      names(spatvec.coords) <- c("lon", "lat")
+      coords.spatvec.df <- data.frame(terra::as.data.frame(coords.spatvec),
+                                      spatvec.coords)
+    }
+
+    out <- merge(coords.spatvec.df, out, by.x =  "ID_coords", by.y = "ID", all = TRUE)
+
+    ## Real climatic values
+    out[,climatic_var_single] <- out[,climatic_var_single]/100
+
+    invisible(out)
+
+  }
+
+  ## If output == "raster", return a cropped raster
+  if (output == "raster") {
+
+    if (terra::geomtype(coords.spatvec) == "polygons") {
+      out <- terra::crop(rasters.sub, coords.spatvec, mask = TRUE)
+      out_NA <- terra::crop(rasters_NA.sub, coords.spatvec, mask = TRUE)
+    } else {
+      out <- terra::crop(rasters.sub, coords.spatvec)
+      out_NA <- terra::crop(rasters_NA.sub, coords.spatvec)
+    }
+
+    ## Real climatic values
+    out <- out/100
+
+    invisible(list(out, out_NA))
+
+  }
+
+}
+
+
+
+
+period_to_month <- function(period) {
+
+  stopifnot(length(period) >= 1)
+
+  ## period as a number
+
+  if (is.numeric(period)) {
+
+    ini <- period[c(TRUE, diff(period) != 1)]
+    fin <- period[c(diff(period) != 1, TRUE)]
+    ini.fin <- data.frame(ini = paste0(ini, "-01-01"),
+                          fin = paste0(fin, "-12-01"))
+
+    months.list <- lapply(split(ini.fin, seq_len(nrow(ini.fin))),
+                        function(x) {
+                          seq.Date(from = as.Date(x$ini),
+                                   to = as.Date(x$fin),
+                                   by = "months")})
+    months <- do.call("c", months.list)
+    names(months) <- NULL
+
+  }
+
+  ## period as character, e.g. "2005-01"
+
+  if (is.character(period)) {
+
+    ini <- do.call(rbind, strsplit(period, split = ":"))[,1]
+
+    if (ncol(do.call(rbind, strsplit(period, split = ":"))) == 2) {
+
+      fin <- do.call(rbind, strsplit(period, split = ":"))[,2]
+
+    } else {
+
+      fin <- ini
+
+    }
+
+    ini.fin <- data.frame(ini = paste0(ini, "-01"),
+                          fin = paste0(fin, "-01"))
+
+    months.list <- lapply(split(ini.fin, seq_len(nrow(ini.fin))),
+                        function(x) {
+                          seq.Date(from = as.Date(x$ini),
+                                   to = as.Date(x$fin),
+                                   by = "month")})
+
+    months <- do.call("c", months.list)
+
+    names(months) <- NULL
+
+  }
+
+  invisible(months)
+
+}
+
+
+
---
+++ R/reshape_terra_extract.R
@@ -0,0 +1,37 @@
+
+
+#' Reshape terra output from get_montly_climate_single and get_daily_climate_single
+#' when output = "df"
+#'
+#' @param df.wide Character. Climatic variable to be downloaded.
+#' One of 'Tmax', 'Tmin' or 'Prcp'.
+#' @param climvar A data.frame with the output of terra::extract.
+#'
+#' @return A data.frame
+#'
+#' @keywords internal
+#' @noRd
+#'
+#' @author Veronica Cruz-Alonso, Francisco Rodriguez-Sanchez
+
+
+reshape_terra_extract <- function(df.wide, climvar) {
+
+  df.wide <- df.wide[,!names(df.wide) %in% c("lon", "lat")]
+  names(df.wide)[names(df.wide) %in% c("x", "y")] <- c("lon", "lat")
+
+  names(df.wide)[!names(df.wide) %in% c("ID", "lon", "lat")] <-
+    paste0(climvar, ".", names(df.wide)[!names(df.wide) %in% c("ID", "lon", "lat")])
+
+  df.long <- stats::reshape(df.wide, direction = "long",
+                            idvar = c("ID", "lon", "lat"),
+                            varying = names(df.wide)[!names(df.wide) %in%
+                                                       c("ID", "lon", "lat")],
+                            timevar = "date")
+
+  df.long <- df.long[order(df.long$ID, df.long$date), ]
+  row.names(df.long) <- NULL
+
+  df.long
+
+}
